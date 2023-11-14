2eee4d801993c585ca3e6b1afe8651a789a804c1
ariclenespedro <pedroariclenes@gmail.com>
2023-11-14 14:31:25 +0100

    Passando evendos por props

--- src/components/Evento.js
+++ src/components/Evento.js
@@ -1,14 +1,17 @@
-function click(texto) {
-    document.getElementById('texto').innerText = texto;
-
-}
+import Button from "./eventos/Button";
 
 function Evento() {
+
+    function click() {
+        document.getElementById('texto').innerText = "Tem uma nova informação no wallboard ";
+    
+    }
     return (
         <div>
             <p>Click para ver</p>
-            <button onClick={() => click("Você clicou no botão")}>botão</button>
+            {/* <button onClick={() => click("Você clicou no botão")}>botão</button> */}
             <p id="texto"></p>
+            <Button event={click} value="Novo Walboard"/>
         </div>
     )
 }
