4c3fccba6078cda9b311731143fe1a1be74f2ddf
ariclenespedro <pedroariclenes@gmail.com>
2023-11-15 17:01:50 +0100

    Criando rotas em react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 /* import logo from './logo.svg'; */
 import './App.css';
-import NewList from './components/NewList';
-import OutraLista from './components/OutraLista';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from "./pages/Home";
 
 function App() {
 
-  const listItem = ['React', 'Vue', 'Angular'];
-
   return (
-    <div className="App">
-      <h1>Renderização de Lista</h1>
-      <OutraLista />
-      <NewList />
-    </div>
+    /* <div className="App"></div> */
+    <Router>
+      <ul>
+        <li>
+          <Link to="/"> Home</Link>
+        </li>
+        <li>
+          <Link to="/empresa"> Empresa</Link>
+        </li>
+        <li>
+          <Link to="/contactos"> Contacto </Link>
+        </li>
+      </ul>
+
+
+      <Routes>
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Contactos.js
@@ -0,0 +1,11 @@
+
+function Contactos() {
+  return (
+    <div>
+        <h1>Contactos</h1>
+        <p>Conteúdo da página</p> 
+    </div>
+  )
+}
+
+export default Contactos
